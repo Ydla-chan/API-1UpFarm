00b6bb9fc72ac10d38456a7bd045c5aa96cc1b2a
Bayu Maulana <77946987+bayuzzs@users.noreply.github.com>
2024-12-02 01:31:46 +0700

    restructure folder

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const router = require('./router/router');  
-const response = require('./response/response');  
+const router = require('./router/router');
+const response = require('./response/response');
 const app = express();
 const port = 3000;
 
 // Middleware
 app.use(bodyParser.json());
 
-
-// Route Awal ApI 
+// Route Awal ApI
 app.get('/', (req, res) => {
-   response.success('Selamat datang di API 1Upfarm by Cangkul titik Koma', res);  
- });
+  response.success('Selamat datang di API 1Upfarm by Cangkul titik Koma', res);
+});
 
-
-// Routing API 
+// Routing API
 router(app);
 
 // Start the server
